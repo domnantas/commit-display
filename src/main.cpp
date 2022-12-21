6cb2c2ee7f6847440598488f5ca44e82b0833b35
domnantas <hello@domnantas.lt>
2022-12-22 00:07:45 +0200

    Draw loading indicator

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
                                                NEO_GRB + NEO_KHZ800);
 
 const uint16_t color_level_0 = matrix.Color(0, 0, 0);
-const uint16_t color_level_1 = matrix.Color(0, 5, 0);
+const uint16_t color_level_1 = matrix.Color(0, 10, 0);
 const uint16_t color_level_2 = matrix.Color(0, 30, 0);
-const uint16_t color_level_3 = matrix.Color(0, 90, 0);
-const uint16_t color_level_4 = matrix.Color(0, 180, 0);
+const uint16_t color_level_3 = matrix.Color(0, 50, 0);
+const uint16_t color_level_4 = matrix.Color(0, 100, 0);
 
 std::map<String, uint16_t> color_map = {
     {"NONE", color_level_0},
@@ -46,19 +46,65 @@ String get_ISO8601_time(time_t timestamp)
   return str;
 }
 
-void init_WiFi()
+void draw_loading_indicator(int step)
+{
+  matrix.fill(0);
+  switch (step)
+  {
+  case 0:
+    matrix.drawPixel(3, 3, color_level_4);
+    matrix.drawPixel(4, 3, color_level_3);
+    matrix.show();
+    break;
+  case 1:
+    matrix.drawPixel(3, 4, color_level_4);
+    matrix.drawPixel(3, 3, color_level_3);
+    break;
+  case 2:
+    matrix.drawPixel(4, 4, color_level_4);
+    matrix.drawPixel(3, 4, color_level_3);
+    break;
+  case 3:
+    matrix.drawPixel(4, 3, color_level_4);
+    matrix.drawPixel(4, 4, color_level_3);
+    break;
+  default:
+    return;
+  }
+  matrix.show();
+}
+
+int init_WiFi()
 {
   WiFi.mode(WIFI_STA);
+  Serial.print("Connecting to WiFi");
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to WiFi");
+  int attempt_count = 0;
+
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print('.');
+    Serial.print(".");
+    draw_loading_indicator(attempt_count % 4);
     delay(1000);
+    attempt_count++;
+
+    if (WiFi.status() == WL_NO_SSID_AVAIL)
+    {
+      Serial.println("SSID not found");
+      return 1;
+    }
+
+    if (WiFi.status() == WL_CONNECT_FAILED || attempt_count > 20)
+    {
+      Serial.println("WIFI connection failed.");
+      return 1;
+    }
   }
   Serial.print(" Connected! IP: ");
   Serial.println(WiFi.localIP());
+  return 0;
 }
 
 JsonArray get_github_contribution_weeks(time_t from, time_t to)
@@ -141,11 +187,16 @@ void setup()
   Serial.begin(115200);
 
   matrix.begin();
+  matrix.setTextWrap(false);
   matrix.setBrightness(255);
   matrix.fillScreen(0);
   matrix.show();
 
-  init_WiFi();
+  while (init_WiFi() != 0)
+  {
+    Serial.println("Retrying in 10s...");
+    delay(10000);
+  }
 
   const char *ntpServer = "pool.ntp.org";
   configTime(0, 0, ntpServer);
